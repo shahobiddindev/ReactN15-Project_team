5f8705759998e428d2eb9e4f0c0633027c23ff62
“milliybro” <shohrux-rustamov@mail.ru>
2023-07-09 18:32:10 +0500

    join res

--- js/main.js
+++ js/main.js
@@ -9,3 +9,18 @@ function openNavbar() {
  
  document.getElementById("navbar-open").addEventListener("click", openNavbar);
  document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+ 
+
+
+ function openNavbar() {
+   document.getElementById("join-responsive").style.left = "0";
+   // document.getElementById("navbar-responsive").style.top = "0";
+ }
+ function closeNavbar() {
+   document.getElementById("join-responsive").style.left = "-100%";
+   // document.getElementById("navbar-responsive").style.top = "-100%";
+ }
+ 
+ document.getElementById("join-open").addEventListener("click", openNavbar);
+ document.getElementById("join-close").addEventListener("click", closeNavbar);
+ 
